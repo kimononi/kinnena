3d8d255d5361bb2f550b1085242dd212fe37ff61
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-10 19:47:04 +0700

    fix Cannot read properties of undefined error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import {
 } from "http/mod.ts";
 
 await serve(async (request) => {
-  const pathnames = request.url.origin.pathname.split("/");
+  const pathnames = request.url.pathname.split("/");
   return new Response("Hello World!");
 });
