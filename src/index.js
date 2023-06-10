f25340805dafa5ae4fe6d57cd4c16427374e9965
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-10 19:45:39 +0700

    fix eequest is not defined error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import {
 } from "http/mod.ts";
 
 await serve(async (request) => {
-  const pathnames = eequest.url.origin.pathname.split("/");
+  const pathnames = request.url.origin.pathname.split("/");
   return new Response("Hello World!");
 });
