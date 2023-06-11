1485e1cc2f28fbdd8aade82cc8360cb21b9c0bea
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-11 17:28:58 +0700

    handle home route

---
+++ src/routes/home.js
@@ -0,0 +1,13 @@
+import * as routes from "./mod.js";
+
+export const data = {
+  method: "GET"
+};
+
+export async function execute(requestEvent) {
+  const filteredRoutes = Object.entries(routes)
+    .filter(([key, ctx]) => ctx.data.method == "GET");
+  //  && ctx.data.pathname !== "" < additional filter
+  const mappedRoutes = Object.fromEntries(filteredRoutes.map(([key, ctx]) => [key, `${requestEvent.request.url + (ctx.data.pathname ? `/${ctx.data.pathname}` : "")}`]))
+  return new Response(JSON.stringify(mappedRoutes, null, "  "))
+};
