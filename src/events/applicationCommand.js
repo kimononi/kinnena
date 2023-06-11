23e617f719b2c4b4db8fc160d92a764e40bde5b5
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-11 17:22:33 +0700

    handle applicationCommand

---
+++ src/events/applicationCommand.js
@@ -0,0 +1,19 @@
+import { 
+  InteractionType
+} from "discord_api_types/v10.ts";
+
+import * as commands from "../commands/mod.js";
+
+export const data = {
+  type: InteractionType.ApplicationCommand
+};
+
+export async function execute({ request, interaction }) {
+  console.log("application command used.");
+  
+  const command = Object.values(commands)
+    .find(cmd => cmd.data.name == interaction.data.name && cmd.data.type == interaction.data.type);
+  console.log(command);
+  
+  if (command) return await command.execute({ request, interaction });
+};
