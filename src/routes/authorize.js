59f268c5613cb4c039b5f9a3b0b7d0ae683c1afd
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-16 09:48:55 +0700

    move oauth function from index to routes/authorize

--- src/routes/authorize.js
+++ src/routes/authorize.js
@@ -3,10 +3,21 @@ export const data = {
   pathname: "/authorize",
 };
 
+const scopes = [OAuth2Scopes.Identify];
+const authorizeURL = new URL(OAuth2Routes.authorizationURL);
+
+authorizeURL.searchParams.set("client_id", Deno.env.get("PROD_DISCORD_ID"));
+authorizeURL.searchParams.set("redirect_uri", requestURL.origin + data.pathname);
+authorizeURL.searchParams.set("response_type", "code");
+authorizeURL.searchParams.set("scope", scopes.join(" "));
+
+export const authorizeURL = authorizeURL;
+
 export async function execute({ requestURL }) {
   const body = new URLSearchParams();
 
   body.set("client_id", Deno.env.get("PROD_DISCORD_ID"));
   body.set("client_secret", Deno.env.get("PROD_DISCORD_SECRET"));
   body.set("code", requestURL.searchParams.get("code"));
+  body.set("grant_type", "authorization_code");
 };
