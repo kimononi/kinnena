79d69703b1fb3c4e337f16e1c4226df6d440d857
Amu <117264067+salmoenela@users.noreply.github.com>
2023-06-16 07:36:14 +0700

    update home route pathname data

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,13 +2,13 @@ import * as routes from "./mod.js";
 
 export const data = {
   method: "GET",
-  pathname: ""
+  pathname: "/"
 };
 
 export async function execute(request) {
   const filteredRoutes = Object.entries(routes)
     .filter(([key, ctx]) => ctx.data.method == "GET");
-  //  && ctx.data.pathname !== "" < additional filter
+  
   const mappedRoutes = Object.fromEntries(filteredRoutes.map(([key, ctx]) => [key, `${request.url + (ctx.data.pathname || "")}`]))
   return new Response(JSON.stringify(mappedRoutes, null, "  "))
 };
