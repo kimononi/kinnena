1b48edd34ac4059bdc55b3c651846ebf8561fc03
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-16 14:38:38 +0700

    remove command debug

--- src/events/applicationCommand.js
+++ src/events/applicationCommand.js
@@ -11,7 +11,6 @@ export const data = {
 export async function execute({ branch, request, interaction }) {
   const command = Object.values(commands)
     .find(cmd => cmd.data.name == interaction.data.name && cmd.data.type == interaction.data.type);
-  console.log(command);
   
   if (command) return await command.execute({ branch, request, interaction });
 };
