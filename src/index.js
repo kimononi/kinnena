4352d3cd60fd6e0f49c4f30915d9b9e56d84d3c4
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-16 07:17:58 +0700

    rewrite routes handler

--- src/index.js
+++ src/index.js
@@ -3,25 +3,9 @@ import * as routes from "./routes/mod.js";
 
 async function handler(request) {
   const requestURL = new URL(request.url);
-  const pathnames = requestURL.pathname.split("/");
-      
-  const deployment = Deno.env.get("DENO_DEPLOYMENT_ID");
-  console.log(deployment, pathnames);
-      
-  if (!requestURL.host.includes(deployment)) {
-    const status = Status.Unauthorized;
-    return new Response(
-      JSON.stringify({
-        message: `This deployment is private, you need a deployment id to access the server (${status})`
-      }, null, "  "),
-      { status }
-    );
-  } else {
-    const route = Object.values(routes)
-      .find(ctx => ctx.data.pathname == pathnames[1] && ctx.data.method == request.method);
-      console.log(route);
-      if (route) return await route.execute(request);
-    }
+  const pathnames = requestURL.pathname.slice(1).split("/");
+
+  return new Response("currently in development.");
 };
 
 serve(handler, { port: 80 });
