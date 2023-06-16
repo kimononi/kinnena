f112cabc98e03fc70e39994d0b97139c8f7a5099
Amu <117264067+salmoenela@users.noreply.github.com>
2023-06-16 07:33:33 +0700

    set unknown route response

--- src/index.js
+++ src/index.js
@@ -12,9 +12,11 @@ import * as routes from "./routes/mod.js";
 async function handler(request) {
   const requestURL = new URL(request.url);
   const branch = (requestURL.host.includes("--") || requestURL.host.includes(Deno.env.get("DENO_DEPLOYMENT_ID"))) ? "CANARY" : "PROD";
-  const pathnames = requestURL.pathname.slice(1).split("/");
 
-  return new Response("currently in development.");
+  const route = Object.values(routes) 
+    .find(ctx => ctx.data.pathname == requestURL.pathname || ctx.data.pathname.startsWith(requestURL.pathname) && ctx.data.method == request.method);
+
+  if (!route) return new Response("Unknown endpoint.", { status: Status.NotFound });
 };
 
 serve(handler, { port: 80 });
