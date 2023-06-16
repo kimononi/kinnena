3b766bbb7006ff9c6d0521c59428de23f5894235
Amu <117264067+salmoenela@users.noreply.github.com>
2023-06-16 07:26:40 +0700

    define request branch

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import * as routes from "./routes/mod.js";
 
 async function handler(request) {
   const requestURL = new URL(request.url);
+  const branch = (requestURL.host.includes("--") || requestURL.host.includes(Deno.env.get("DENO_DEPLOYMENT_ID"))) ? "CANARY" : "PROD";
   const pathnames = requestURL.pathname.slice(1).split("/");
 
   return new Response("currently in development.");
