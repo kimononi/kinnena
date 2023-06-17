e14408a421d52734585de77a5afed07212561819
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-17 17:06:16 +0700

    create a test route

---
+++ src/routes/test.js
@@ -0,0 +1,32 @@
+import {
+  ButtonStyle,
+  ComponentType,
+  RouteBases,
+  Routes
+} from "discord_api_types/v10.ts";
+
+export const data = {
+  method: "GET",
+  pathname: "/deploy",
+  requireAuth: true
+};
+
+export async function execute({ branch }) {
+  const clientToken = Deno.env.get(`${branch}_DISCORD_TOKEN`);
+  
+  const body = JSON.stringify({
+    content: "# Verifikasi! {emoji}\n\n* Sebelum verifikasi, pastiin kamu sudah baca semua peraturan disini dan siap menerima peringatan atau tindakan dari admin apabila melanggar."
+  });
+  
+  const testResult = await fetch(RouteBases.api + Routes.channelMessages(""), {
+    method: "POST",
+    headers: {
+      authorization: `Bot ${clientToken}`,
+      "content-type": "application/json"
+    },
+    body
+  })
+    .then(res => res.json());
+  
+  return new Response(JSON.stringify(testResult, null, "  "))
+};
