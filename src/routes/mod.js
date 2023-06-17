a0538f9df8fd10b344200ec3ea19f60d8ecdf23a
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-17 17:08:03 +0700

    export test route

--- src/routes/mod.js
+++ src/routes/mod.js
@@ -2,3 +2,4 @@ export * as authorize from "./authorize.js";
 export * as deploy from "./deploy.js";
 export * as home from "./home.js";
 export * as interaction from "./interaction.js";
+export * as test from "./test.js";
