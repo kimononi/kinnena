3b4e3d6b97c7d27c624336c9a0a8748ac0a56864
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-22 15:38:50 +0700

    verification system handler

--- src/components/verification.js
+++ src/components/verification.js
@@ -11,6 +11,9 @@ export async function execute({ branch, interaction }) {
   const minCreationTime = 24 * 60 * 60 * 1000;
 
   const systems = [
-    { guildId: "", verificationRoleId: "", flaggedRoleId: "" }
+    { guildId: "1100208880132309022", verificationRoleId: "1119566549213266011", flaggedRoleId: "1121351938722758737" }
   ];
+
+  const system = systems.find(ctx => ctx.guildId === interaction.guild_id);
+  if (!system) return;
 };
