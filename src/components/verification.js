c11e5394a75c3c887fbd980a4a0f6e309681baf8
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-22 17:04:20 +0700

    handle verification result

--- src/components/verification.js
+++ src/components/verification.js
@@ -43,6 +43,20 @@ export async function execute({ branch, interaction }) {
       headers: { "content-type": "application/json" }
     });
   } else {
+    const result = await fetch(RouteBases.api + Routes.guildMemberRole(interaction.guild_id, interaction.member.user.id, system.verificationRole), {
+      method: "PUT",
+      headers: {
+        authorization: `Bot ${branch}_DISCORD_TOKEN`
+      }
+    })
+      .then(res => res.json());
 
+    return new Response(JSON.stringify({
+      type: InteractionResponseType.ChannelMessageWithSource,
+      data: {
+        flags: MessageFlags.Ephemeral,
+        content: ("error" in result) ? `Wah, ada yang ga beres nih, tolong laporin bug ini ke moderator ya..\n\n${JSON.stringify(result, null, "    ")}` : "Arigatouu, thank you udah verifikasi, selamat bergabung~"
+      }
+    }));
   }
 };
