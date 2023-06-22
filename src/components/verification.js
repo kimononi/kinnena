3ad547e85157acaf54025022b6891100a95b9944
Kin~ <120244424+kinnxna@users.noreply.github.com>
2023-06-22 15:46:50 +0700

    handle unverified user

--- src/components/verification.js
+++ src/components/verification.js
@@ -1,5 +1,7 @@
 import {
-  ComponentType
+  ComponentType,
+  InteractionResponseType,
+  MessageFlags
 } from "discord_api_types/v10.ts";
 
 export const data = {
@@ -11,9 +13,19 @@ export async function execute({ branch, interaction }) {
   const minCreationTime = 24 * 60 * 60 * 1000;
 
   const systems = [
-    { guildId: "1100208880132309022", verificationRoleId: "1119566549213266011", flaggedRoleId: "1121351938722758737" }
+    { guildId: "1100208880132309022", verificationRole: "1119566549213266011", flaggedRole: "1121351938722758737" }
   ];
 
   const system = systems.find(ctx => ctx.guildId === interaction.guild_id);
-  if (!system) return;
+  if (!system || interaction.member.roles.includes(system.verificationRole)) return;
+
+  if (interaction.member.roles.includes("")) return new Response(JSON.stringify({
+    type: InteractionResponseType.ChannelMessageWithSource,
+    data: {
+      flags: MessageFlags.Ephemeral,
+      content: "belum disetup"
+    }
+  }), {
+    headers: { "content/type": "application/json" }
+  });
 };
